ad9a8e9c6f403277e03bd85d084e35e926530919
Joel <jjshoe>
2020-06-18 09:43:07 -0500

    Add NotString method, add examples

--- grep.go
+++ grep.go
@@ -4,12 +4,25 @@ import "regexp"
 
 // String returns a list of matches from the pattern you pass in
 func String(pattern string, list []string) []string {
+	return _string(pattern, list, false)
+}
+
+// NotString returns a list of strings that DONT match the pattern you pass in
+func NotString(pattern string, list []string) []string {
+	return _string(pattern, list, true)
+}
+
+func _string(pattern string, list []string, negateMatch bool) []string {
 	var matches []string
 
 	re := regexp.MustCompile(pattern)
 
 	for _, item := range list {
 		if re.Match([]byte(item)) {
+			if negateMatch == false {
+				matches = append(matches, item)
+			}
+		} else if negateMatch {
 			matches = append(matches, item)
 		}
 	}
